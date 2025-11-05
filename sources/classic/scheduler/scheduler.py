e4ce9210f14f086e4bcb31c8c6b3d27d0dde6961
Sergey Variasov <variasov@gmail.com>
2025-11-05 20:11:13 +0700

    обновление actors

--- sources/classic/scheduler/scheduler.py
+++ sources/classic/scheduler/scheduler.py
@@ -4,7 +4,6 @@ from concurrent.futures import ThreadPoolExecutor
 from dataclasses import dataclass
 from datetime import datetime, timedelta, timezone
 from queue import Empty, Queue
-from threading import Thread
 from typing import Any, Callable, Union, Optional
 
 from classic.actors import Actor, Stop
@@ -26,8 +25,6 @@ class Scheduler(Registry, Actor):
     Планировщик задач. Вызывает переданные ему Callable объекты в соответствии
     с расписанием.
     """
-    _stopped: bool
-    _thread: Optional[Thread]
     _thread_pool: Optional[ThreadPoolExecutor]
 
     def __init__(self, workers_num: int = 1) -> None:
@@ -60,18 +57,6 @@ class Scheduler(Registry, Actor):
     def _schedule(self, task: Task) -> None:
         heapq.heappush(self._tasks, task)
 
-    def _cancel(self, task_name: str) -> None:
-        """
-        Отменяет выполнение запланированной задачи.
-
-        Args:
-            task_name (str): Имя задачи для отмены.
-        """
-        tasks = [t for t in self._tasks if t.name != task_name]
-        if len(self._tasks) != len(tasks):
-            heapq.heapify(tasks)
-            self._tasks = tasks
-
     def _execute_task(self, task: Task) -> None:
         """
         Выполняет текущую задачу.
@@ -84,35 +69,31 @@ class Scheduler(Registry, Actor):
         else:
             task.run_job()
 
-    def run(self) -> None:
+    def _loop(self) -> None:
         """
         Главный цикл выполнения планировщика.
         """
-        self._stopped = False
-        while not self._stopped:
+        timeout = self._calc_next_task_execute_time()
+        if timeout > 0:
             try:
-                timeout = self._calc_next_task_execute_time()
-                if timeout > 0:
-                    try:
-                        command = self._inbox.get(block=True, timeout=timeout)
-                    except Empty:
-                        continue
+                command = self._inbox.get(block=True, timeout=timeout)
+            except Empty:
+                return
 
-                    if isinstance(command, Task):
-                        self._schedule(command)
-                    elif isinstance(command, CancelTask):
-                        self._cancel(command.task_name)
-                    elif isinstance(command, Stop):
-                        self._stop(command)
-                else:
-                    task: Task = heapq.heappop(self._tasks)
-                    task.set_next_run_time()
-                    if task.next_run_time:
-                        self._schedule(task)
-                    self._execute_task(task)
-            except KeyboardInterrupt:
+            if isinstance(command, Task):
+                self._schedule(command)
+            elif isinstance(command, CancelTask):
+                self._cancel(command.task_name)
+            elif isinstance(command, Stop):
                 self._stop()
+        else:
+            task: Task = heapq.heappop(self._tasks)
+            task.set_next_run_time()
+            if task.next_run_time:
+                self._schedule(task)
+            self._execute_task(task)
 
+    def _after_loop(self):
         if self._thread_pool:
             self._thread_pool.shutdown(wait=True, cancel_futures=True)
 
@@ -211,6 +192,18 @@ class Scheduler(Registry, Actor):
         """
         cancellation = CancelTask(task_name=task_name)
         self._inbox.put(cancellation)
+
+    def _cancel(self, task_name: str) -> None:
+        """
+        Отменяет выполнение запланированной задачи.
+
+        Args:
+            task_name (str): Имя задачи для отмены.
+        """
+        tasks = [t for t in self._tasks if t.name != task_name]
+        if len(self._tasks) != len(tasks):
+            heapq.heapify(tasks)
+            self._tasks = tasks
 
     @staticmethod
     def _task_name_for_method(
